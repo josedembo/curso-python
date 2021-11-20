c7830bbe2b627060fb077a39a462dd885f45f9fa
josedembo <josedembo18@aluno.unilab.edu.br>
2021-11-20 04:20:17 -0300

    adiciona primeiro aqruivo python

---
+++ meu_app.py
@@ -0,0 +1,10 @@
+print("Hello world form Braisl")
+nome = input("Qual é o seu nome meu amigo\nR:");
+print(f"Prazer em conhecelo {nome}");
+print(f"{nome} vamos fazer um jogo, digite dois numeros em seguida");
+a = int(input("digite aqui o primero valor\nR:"));
+b = int(input("digite aqui o segundo valor\nR:"));
+
+soma = a + b;
+
+print(f"{nome} a soma entre {a} e {b} = {soma}");
